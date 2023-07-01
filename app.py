e685a770bb157a05d2de322e23a3a2debf556914
TanyaShp <tanyashpichka@gmail.com>
2023-06-30 19:59:40 -0700

    added review functionality, major db changes

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ import string
 import random
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -52,15 +52,25 @@ class User(UserMixin, db.Model):
     books = db.relationship("Book", backref="user", lazy=True)
 
 
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=True)
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship("User", backref=db.backref("reviews", lazy=True))
+
+
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=True)
     author = db.Column(db.String(100), nullable=True)
-    review = db.Column(db.String(500), nullable=True)
     image_file = db.Column(db.String(120), nullable=True, default="default.jpg")
     user_id = db.Column(
         db.Integer, db.ForeignKey("user.id", name="fk_user_id"), nullable=True
     )
+    reviews = db.relationship("Review", backref="book", lazy=True)
 
     def __repr__(self):
         return "<Book %r>" % self.title
@@ -152,7 +162,6 @@ def add_book():
     if request.method == "POST":
         title = request.form["title"]
         author = request.form["author"]
-        review = request.form["review"]
 
         # Check for the cropped_image in the request files
         if "cropped_image" in request.files:
@@ -178,11 +187,17 @@ def add_book():
         new_book = Book(
             title=title,
             author=author,
-            review=review,
             image_file=filename,
             user_id=current_user.id,
         )
         db.session.add(new_book)
+        db.session.flush()
+
+        review_content = request.form["review"]
+        new_review = Review(
+            content=review_content, book_id=new_book.id, user_id=current_user.id
+        )
+        db.session.add(new_review)
         db.session.commit()
 
         return jsonify({"location": url_for("index")}), 201
@@ -216,38 +231,30 @@ def edit_book(book_id):
             if request.method == "POST":
                 title = request.form["title"]
                 author = request.form["author"]
-                review = request.form["review"]
+
+                img_data = None
+                filename = "default.jpg"
 
                 # Check for the cropped_image in the request files
                 if "cropped_image" in request.files:
                     img_data = request.files["cropped_image"]
-
-                    # Generate a unique filename
-                    filename = generate_unique_filename(title)
-
-                    # Save the image
-                    img_data.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-                # Check for the original_image in the request files
+                # If no cropped_image, check for the original_image in the request files
                 elif "original_image" in request.files:
                     img_data = request.files["original_image"]
 
+                if img_data and allowed_file(img_data.filename):
                     # Generate a unique filename
                     filename = generate_unique_filename(title)
 
                     # Save the image
                     img_data.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-                else:
-                    filename = "default.jpg"
 
                 book_to_edit.title = title
                 book_to_edit.author = author
-                book_to_edit.review = review
                 book_to_edit.image_file = filename
 
                 db.session.commit()
-
-                flash("Book review has been updated!", "success")
-                return jsonify({"location": url_for("index")}), 200
+                return jsonify({"location": url_for("index")})  # Redirect to index page
             else:
                 return render_template(
                     "edit_book.html",
@@ -258,6 +265,77 @@ def edit_book(book_id):
             abort(403)  # Unauthorized action
     else:
         abort(404)  # Book not found
+
+
+@app.route("/book_reviews/<int:book_id>")
+def book_reviews(book_id):
+    book = Book.query.get_or_404(book_id)  # Fetch the book from the database
+    if book:
+        return render_template("book_reviews.html", book=book)
+    else:
+        abort(404)  # Book not found
+
+
+@app.route("/add_review/<int:book_id>", methods=["GET", "POST"])
+@login_required
+def add_review(book_id):
+    book = Book.query.get(book_id)
+    if book:
+        if request.method == "POST":
+            content = request.form.get("content")
+            new_review = Review(
+                content=content, book_id=book_id, user_id=current_user.id
+            )
+            db.session.add(new_review)
+            db.session.commit()
+            flash("Review has been added!", "success")
+            return redirect(url_for("book_reviews", book_id=book_id))
+        else:
+            return render_template("add_review.html", book=book)
+    else:
+        abort(404)  # Book not found
+
+
+@app.route("/edit_review/<int:review_id>", methods=["GET", "POST"])
+@login_required
+def edit_review(review_id):
+    review_to_edit = Review.query.get(review_id)
+
+    # Make sure the review exists and belongs to the current user or the user is an admin
+    if review_to_edit and (
+        review_to_edit.user_id == current_user.id or current_user.is_admin
+    ):
+        if request.method == "POST":
+            content = request.form["content"]
+
+            review_to_edit.content = content
+
+            db.session.commit()
+
+            flash("Review has been updated!", "success")
+            return redirect(url_for("index"))  # Redirect to index page
+        else:
+            return render_template(
+                "edit_review.html",
+                review=review_to_edit,
+                action_url=url_for("edit_review", review_id=review_id),
+            )
+    else:
+        abort(403)  # Unauthorized action
+
+
+@app.route("/delete_review/<int:review_id>/<int:book_id>", methods=["POST"])
+@login_required
+def delete_review(review_id, book_id):
+    review = Review.query.get_or_404(review_id)
+    if not current_user.is_admin and (
+        review.user_id != current_user.id or review.book_id != book_id
+    ):
+        abort(403)
+    db.session.delete(review)
+    db.session.commit()
+    flash("Your review has been deleted.", "success")
+    return redirect(url_for("book_reviews", book_id=book_id))
 
 
 if __name__ == "__main__":
